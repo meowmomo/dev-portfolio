b54ed75ac04f962b9ee4538dfb051a81fce5cd86
Kitty Kio <205472723+modularmanul@users.noreply.github.com>
2026-02-06 15:32:36 +0900

    Size down mongodb icon

--- src/icons/MongoDBIcon.tsx
+++ src/icons/MongoDBIcon.tsx
@@ -4,8 +4,8 @@ export const MongoDBIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
-      width="100"
-      height="100"
+      width="35"
+      height="35"
       viewBox="0 0 48 48"
     >
       <path
